cacc57f48bde63d378dd69d75cbefa5ad980ca86
Ismael Castro <ismael.castro@atos.net>
2018-04-30 14:05:19 +0200

    initial upload

---
+++ src/basic/test.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class test {
+	
+	public static void main(String args[]) throws Exception {
+		try {
+		} catch (Exception e) {
+			System.out.println("Exception in main");
+		}
+	}
+}
